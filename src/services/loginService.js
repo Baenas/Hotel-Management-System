18a9f34c2ccf167aad7168ad2bd949e3ff8e5558
Baenas <dbg94dani@gmail.com>
2020-05-12 22:27:05 +0200

    little fix

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ApiClient {
   constructor() {
     this.apiClient = axios.create({
-      baseURL: process.env.REACT_APP_API
+      baseURL: process.env.REACT_APP_API_URL
 
     });
   }
